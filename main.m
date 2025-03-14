e7fdbed6cd55846fc8ca574fe90d2b0473a5d995
RubenSouls <rubenalmasbernardino@tutanota.com>
2025-03-14 14:48:34 +0000

    uns erros corrigidos

--- main.m
+++ main.m
@@ -71,7 +71,7 @@ r = [deg2rad(10), deg2rad(20)];
 open("UAV3atitude.slx");
 sim("UAV3atitude.slx");
 
-graficos_gerais(t_lqr,bb_lqr,lambda_lqr,p_lqr,r_lqr,phi_lqr,deltaa_lqr,deltar_lqr);
+graficos_gerais(t_lqr,bb_ref_lqr,lambda_ref_lqr ,bb_lqr,lambda_lqr,p_lqr,r_lqr,phi_lqr,deltaa_lqr,deltar_lqr);
 
 confirmar_valores_finais(r,bb_lqr,lambda_lqr);
 
@@ -94,15 +94,16 @@ Q = diag([10,1,1,1,30,75,75]);
 R = diag([1/deg2rad(15),1/deg2rad(15)]);
 
 K = lqr(A_temp,B_temp,Q,R);
+
 %referencia
 r = [deg2rad(10), deg2rad(20)];
 
 open("UAV3atitude_int.slx");
 sim("UAV3atitude_int.slx");
 
-graficos_gerais(t_int,bb_ref,lambda_ref ,bb_int,lambda_int,p_int,r_int,phi_int,deltaa_int,deltar_int);
+graficos_gerais(t_int,bb_ref_int,lambda_ref_int ,bb_int,lambda_int,p_int,r_int,phi_int,deltaa_int,deltar_int);
 
-confirmar_valores_finais(r,bb_int,lambda_int);
+
 
 
 
